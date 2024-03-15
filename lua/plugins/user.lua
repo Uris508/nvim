046b11ccf8259414e9d6e03c8e06c875bb1a9f6e
Uris Cheng <cheng.uris@inventec.com>
2024-03-15 17:12:56 +0800

    Config Telescope UI

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -24,10 +24,6 @@ if true then
       end,
     },
     {"smartpde/telescope-recent-files"},
-
-    -- {
-    --   "nvim-telescope/telescope-fzf-native.nvim",
-    --   build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-    -- },
+    {'nvim-telescope/telescope-ui-select.nvim' },
   }
 end

ebf15f05f57c5eb9a25e73f389f2bdda25572b3c
Uris Cheng <cheng.uris@inventec.com>
2024-03-16 12:27:57 +0800

    Add keymap for Dashboard and open file folder

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,5 @@ vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
 vim.api.nvim_set_keymap("n", "<Leader>sb",
   [[<cmd>lua require('telescope').extensions.recent_files.pick()<CR>]],
   {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>fo", "<cmd>!start %:p:h<CR>", { silent = true })
+vim.keymap.set("n", "<leader>D", "<cmd>Dashboard<CR>", { silent = true })

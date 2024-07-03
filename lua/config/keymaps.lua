1c868e5de83960951b47da07a46f490d7a075d9f
Uris Cheng <cheng.uris@inventec.com>
2024-07-03 14:43:43 +0800

    Support find files under cusor keymap
    <space>sf

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,3 +16,10 @@ vim.keymap.set("n", "<leader>fN","<cmd>let @+=expand('%:t')<CR>",{ silent = true
 vim.keymap.set("n", "<leader>ch", "<cmd>%!xxd<CR>", { silent = true, desc = "Change to HEX format" })
 vim.keymap.set("n", "<leader>cH", "<cmd>%!xxd -r<CR>", { silent = true, desc = "Change to ASCII format" })
 vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "Open Oil.nvim" })
+local telescopebuiltin = require("telescope.builtin")
+local function grep_cword()
+  return telescopebuiltin.find_files({ default_text = vim.fn.expand('<cword>')})
+end
+vim.keymap.set("n", "<leader>sf", grep_cword, {desc = "Find file under curosr"})
+
+

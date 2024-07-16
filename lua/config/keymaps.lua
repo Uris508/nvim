48b62154dfb4ccf64c020600e0b14d628a88c48a
Uris Cheng <cheng.uris@inventec.com>
2024-07-16 11:52:14 +0800

    fix the custom comment string behavior

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -57,6 +57,7 @@ local function SetCustomCommentString()
 end
 
 local function CustomCommentStringBegin()
+  vim.cmd('normal O')
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   local comment = "[" .. CustomCommentType .. "] " .. CustomCommentStr .. " >>>"
   vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { "[uris]" .. comment })
@@ -64,6 +65,7 @@ local function CustomCommentStringBegin()
 end
 
 local function CustomCommentStringEnd()
+  vim.cmd('normal o')
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   local comment = "[" .. CustomCommentType .. "] " .. CustomCommentStr .. " <<<"
   vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { "[uris]" .. comment })
@@ -71,7 +73,7 @@ local function CustomCommentStringEnd()
 end
 
 vim.keymap.set("n", "<leader>cG", SetCustomCommentString, {desc = "Set Custom Comment string"})
-vim.keymap.set("n", "gcb", CustomCommentStringBegin, {desc = "Custom Comment string"})
-vim.keymap.set("n", "gce", CustomCommentStringEnd, {desc = "Custom Comment string"})
+vim.keymap.set("n", "gcb", CustomCommentStringBegin, {desc = "Custom Comment Begin"})
+vim.keymap.set("n", "gce", CustomCommentStringEnd, {desc = "Custom Comment End"})
 
 

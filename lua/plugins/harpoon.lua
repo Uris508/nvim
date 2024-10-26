4b121c26baef347f8213483bdf02715ca324ffb4
Uris Cheng <cheng.uris@inventec.com>
2024-10-26 11:42:53 +0800

    set harpoon list to dynamic

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,6 +2,9 @@ return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
+  menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+  },
 	config = function()
 		local harpoon = require("harpoon")
 

198f75ba5b56635167a9e66802444011387a7989
Uris Cheng <cheng.uris@inventec.com>
2024-10-26 13:14:56 +0800

    use lazyvim extra harpoon setting

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,43 +1,44 @@
 return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = { "nvim-lua/plenary.nvim" },
-  menu = {
-      width = vim.api.nvim_win_get_width(0) - 4,
-  },
-	config = function()
-		local harpoon = require("harpoon")
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    opts = {
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
+      settings = {
+        save_on_toggle = true,
+      },
+    },
+    keys = function()
+      local keys = {
+        {
+          "<leader>a",
+          function()
+            require("harpoon"):list():add()
+          end,
+          desc = "Harpoon File",
+        },
+        {
+          "<leader>h",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Harpoon Quick Menu",
+        },
+      }
 
-		-- REQUIRED
-		harpoon:setup()
-		-- REQUIRED
-
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():add()
-		end,{silent =true, desc = "add buffer into harpoon list"})
-		vim.keymap.set("n", "<leader>h", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end,{silent =true, desc = "open harpoon list"})
-
-		-- vim.keymap.set("n", "<C-h>", function()
-		-- 	harpoon:list():select(1)
-		-- end)
-		-- vim.keymap.set("n", "<C-t>", function()
-		-- 	harpoon:list():select(2)
-		-- end)
-		-- vim.keymap.set("n", "<C-n>", function()
-		-- 	harpoon:list():select(3)
-		-- end)
-		-- vim.keymap.set("n", "<C-s>", function()
-		-- 	harpoon:list():select(4)
-		-- end)
-		--
-		-- -- Toggle previous & next buffers stored within Harpoon list
-		-- vim.keymap.set("n", "<C-S-P>", function()
-		-- 	harpoon:list():prev()
-		-- end)
-		-- vim.keymap.set("n", "<C-S-N>", function()
-		-- 	harpoon:list():next()
-		-- end)
-	end,
+      for i = 1, 5 do
+        table.insert(keys, {
+          "<leader>" .. i,
+          function()
+            require("harpoon"):list():select(i)
+          end,
+          desc = "Harpoon to File " .. i,
+        })
+      end
+      return keys
+    end,
+  }
 }

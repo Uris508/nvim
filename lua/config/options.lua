243fad9a4343176bae32062bd6e1f8e2697a1174
Uris Cheng <cheng.uris@inventec.com>
2025-03-08 21:50:08 +0800

    feat: support more doomguy face

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,14 +6,20 @@ vim.opt.title = true
 vim.opt.titlestring = 'neovim - ' .. [[%{fnamemodify(getcwd(), ':t')}]]
 vim.g.snacks_animate = true
 
-vim.g.uris_logo = 'chafa c:/logo/doom1.png --format symbols --symbols vhalf --size 40x40'
+vim.g.uris_logo = 'chafa c:/logo/doom/doom3.png --format symbols --size 46x46'
 local hr = tonumber(os.date('%H', os.time()))
-if hr > 6 and hr <= 12 then 
-  vim.g.uris_logo = 'chafa c:/logo/doom1.png --format symbols --symbols vhalf --size 40x40'
-elseif hr > 12 and hr <= 16 then 
-  vim.g.uris_logo = 'chafa c:/logo/doom2.png --format symbols --symbols vhalf --size 40x40'
+if hr > 8 and hr <= 10 then 
+  vim.g.uris_logo = 'chafa c:/logo/doom/doom1.png --format symbols --symbols vhalf --size 40x40'
+elseif hr > 10 and hr <= 12 then 
+  vim.g.uris_logo = 'chafa c:/logo/doom/doom2.png --format symbols --symbols vhalf --size 40x40'
+elseif hr > 12 and hr <= 14 then 
+  vim.g.uris_logo = 'chafa c:/logo/doom/doom3.png --format symbols --symbols vhalf --size 40x40'
+elseif hr > 14 and hr <= 16 then 
+  vim.g.uris_logo = 'chafa c:/logo/doom/doom4.png --format symbols --symbols vhalf --size 40x40'
 elseif hr > 16 and hr <= 18 then 
-  vim.g.uris_logo = 'chafa c:/logo/doom3.png --format symbols --symbols vhalf --size 40x40'
+  vim.g.uris_logo = 'chafa c:/logo/doom/doom5.png --format symbols --symbols vhalf --size 40x40'
+elseif hr > 18 and hr <= 21 then 
+  vim.g.uris_logo = 'chafa c:/logo/doom/doom6.png --format symbols --symbols vhalf --size 40x40'
 else 
-  vim.g.uris_logo = 'chafa c:/logo/doom4.png --format symbols --symbols vhalf --size 40x40'
+  vim.g.uris_logo = 'chafa c:/logo/doom/doom7.png --format symbols --symbols vhalf --size 40x40'
 end

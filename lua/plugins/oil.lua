2dee8b9c7881b4f2c2da6f7743b9edc4233c64c3
Uris Cheng <cheng.uris@inventec.com>
2025-05-25 20:27:37 +0800

    oil: update keymap for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,6 +7,25 @@ return {
     config = function()
       require("oil").setup({
         default_file_explorer = true,
+        use_default_keymaps = false,
+        keymaps = {
+            ["g?"] = { "actions.show_help", mode = "n" },
+            ["<CR>"] = "actions.select",
+            ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+            ["<C-d>"] = { "actions.select", opts = { horizontal = true } },
+            ["<C-t>"] = { "actions.select", opts = { tab = true } },
+            ["<C-p>"] = "actions.preview",
+            ["<C-c>"] = { "actions.close", mode = "n" },
+            ["<C-r>"] = "actions.refresh",
+            ["-"] = { "actions.parent", mode = "n" },
+            ["_"] = { "actions.open_cwd", mode = "n" },
+            ["`"] = { "actions.cd", mode = "n" },
+            ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+            ["gs"] = { "actions.change_sort", mode = "n" },
+            ["gx"] = "actions.open_external",
+            ["g."] = { "actions.toggle_hidden", mode = "n" },
+            ["g\\"] = { "actions.toggle_trash", mode = "n" },
+          },
       })
     end,
   },

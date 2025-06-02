0e4bc0dec6ec88b0ea8257270bbf12656b985970
Uris Cheng <cheng.uris@inventec.com>
2025-06-02 18:11:56 +0800

    remove: remove codeshot

--- lua/plugins/codeshot.lua
+++ lua/plugins/codeshot.lua
@@ -1,9 +1,9 @@
 return {
   -- "SergioRibera/codeshot.nvim",
-  dir = "~/code/nvim-plugin/codeshot.nvim" ,
-  config = function()
-    require('codeshot').setup({
-      silent = false,
-    })
-  end
+  -- dir = "~/code/nvim-plugin/codeshot.nvim" ,
+  -- config = function()
+  --   require('codeshot').setup({
+  --     silent = false,
+  --   })
+  -- end
 }

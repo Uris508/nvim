992669f06fc48c555870cb6657224633cf0baff3
Uris Cheng <cheng.uris@inventec.com>
2025-06-02 18:23:37 +0800

    fix: fix location of doom guy face

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,17 +9,17 @@ vim.g.snacks_animate = true
 vim.g.uris_logo = 'chafa c:/logo/doom/doom3.png --format symbols --size 46x46'
 local hr = tonumber(os.date('%H', os.time()))
 if hr > 8 and hr <= 10 then 
-  vim.g.uris_logo = 'chafa ../nvim/doom/doom1.png --format symbols --symbols vhalf --size 40x40'
+  vim.g.uris_logo = 'chafa %localappdata%/nvim/doom/doom1.png --format symbols --symbols vhalf --size 40x40'
 elseif hr > 10 and hr <= 12 then 
-  vim.g.uris_logo = 'chafa ../nvim/doom/doom2.png --format symbols --symbols vhalf --size 40x40'
+  vim.g.uris_logo = 'chafa %localappdata%/nvim/doom/doom2.png --format symbols --symbols vhalf --size 40x40'
 elseif hr > 12 and hr <= 14 then 
-  vim.g.uris_logo = 'chafa ../nvim/doom/doom3.png --format symbols --symbols vhalf --size 40x40'
+  vim.g.uris_logo = 'chafa %localappdata%/nvim/doom/doom3.png --format symbols --symbols vhalf --size 40x40'
 elseif hr > 14 and hr <= 16 then 
-  vim.g.uris_logo = 'chafa ../nvim/doom/doom4.png --format symbols --symbols vhalf --size 40x40'
+  vim.g.uris_logo = 'chafa %localappdata%/nvim/doom/doom4.png --format symbols --symbols vhalf --size 40x40'
 elseif hr > 16 and hr <= 18 then 
-  vim.g.uris_logo = 'chafa ../nvim/doom/doom5.png --format symbols --symbols vhalf --size 40x40'
+  vim.g.uris_logo = 'chafa %localappdata%/nvim/doom/doom5.png --format symbols --symbols vhalf --size 40x40'
 elseif hr > 18 and hr <= 21 then 
-  vim.g.uris_logo = 'chafa ../nvim/doom/doom6.png --format symbols --symbols vhalf --size 40x40'
+  vim.g.uris_logo = 'chafa %localappdata%/nvim/doom/doom6.png --format symbols --symbols vhalf --size 40x40'
 else 
-  vim.g.uris_logo = 'chafa ../nvim/doom/doom7.png --format symbols --symbols vhalf --size 40x40'
+  vim.g.uris_logo = 'chafa %localappdata%/nvim/doom/doom7.png --format symbols --symbols vhalf --size 40x40'
 end

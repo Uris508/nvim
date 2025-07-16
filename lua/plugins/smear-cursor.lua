f428ce8382f5ee7b1957b2382f2dc87d5067e89d
Uris Cheng <cheng.uris@inventec.com>
2025-07-16 12:50:55 +0800

    fix fine tune the smear-curosr

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -3,25 +3,27 @@ return {
 
   opts = {
     -- Smear cursor when switching buffers or windows.
-    smear_between_buffers = true,
+    smear_between_buffers = false,
 
     -- Smear cursor when moving within line or to neighbor lines.
     -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
     smear_between_neighbor_lines = true,
+    min_horizontal_distance_smear = 5,
+    min_vertical_distance_smear = 2,
 
     -- Draw the smear in buffer space instead of screen space when scrolling
     scroll_buffer_space = true,
 
     -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
     -- Smears will blend better on all backgrounds.
-    legacy_computing_symbols_support = false,
+    -- legacy_computing_symbols_support = true,
 
     -- Smear cursor in insert mode.
     -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
     smear_insert_mode = false,
-    stiffness = 0.8,
-    trailing_stiffness = 0.5,
-    distance_stop_animating = 0.5,
-    -- never_draw_over_target = false,
+    stiffness = 0.9,
+    trailing_stiffness = 0.8,
+    distance_stop_animating = 0.8,
+    -- damping = 0.8,                        -- 0.65     [0, 1]
   },
 }

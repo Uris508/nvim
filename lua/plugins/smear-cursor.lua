d01918d600e528a9fc3a1b2ce383103f27a81eb8
Uris Cheng <cheng.uris@inventec.com>
2025-08-25 11:35:22 +0800

    opt: modify smear-curosr settings

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -5,7 +5,7 @@ return {
     time_interval = 7,
     -- Smear cursor when switching buffers or windows.
     smear_between_buffers = false,
-
+   
     -- Smear cursor when moving within line or to neighbor lines.
     -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
     smear_between_neighbor_lines = true,
@@ -22,13 +22,10 @@ return {
     -- Smear cursor in insert mode.
     -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
     smear_insert_mode = false,
-    stiffness = 0.8,                      -- 0.6      [0, 1]
-    trailing_stiffness = 0.6,             -- 0.45     [0, 1]
-    stiffness_insert_mode = 0.7,          -- 0.5      [0, 1]
-    trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
-    damping = 0.95,                       -- 0.85     [0, 1]
-    damping_insert_mode = 0.95,           -- 0.9      [0, 1]
-    distance_stop_animating = 0.5,        -- 0.1      > 0
+    stiffness = 0.5,
+    trailing_stiffness = 0.8,
+    distance_stop_animating = 0.8,
+    never_draw_over_target = false,
     -- damping = 0.8,                        -- 0.65     [0, 1]
   },
 }

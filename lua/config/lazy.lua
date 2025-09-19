b48cd31e9eb84852647eea2855c13adfd4a62866
Uris Cheng <cheng.uris@inventec.com>
2025-09-19 14:12:00 +0800

    fix: disable rocks for lazy.nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,9 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+  rocks = {
+    enabled = false, -- Disable LuaRocks for this plugin
+  },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {

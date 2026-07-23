c5a702e35b22ce09a5e2218f30899d73368bc94c
Uris Cheng <cheng.uris@inventec.com>
2026-07-23 11:44:17 +0800

    conf: set diffview default to full view

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,9 +1,15 @@
 return {
   "sindrets/diffview.nvim",
    event = "VeryLazy",
-  -- config = function ()
-  --   require("diffview").setup({
-  --
-  --   })
-  -- end
+
+    config = function ()
+      require("diffview").setup({
+        hooks = {
+          -- 在進入 diff 視窗時自動展開所有摺疊
+          diff_buf_read = function(bufnr)
+            vim.opt_local.foldlevel = 99
+          end,
+        },
+      })
+    end
 }

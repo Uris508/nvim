1abb057a963b770447a508b22812117d987f3acd
Uris Cheng <cheng.uris@inventec.com>
2026-07-23 13:26:45 +0800

    feat: support keymap to control view on file_panel
    ]c [c <C-j> <C-k> q

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,11 +3,40 @@ return {
    event = "VeryLazy",
 
     config = function ()
+      local actions = require("diffview.actions")
       require("diffview").setup({
+        keymaps = {
+          file_panel = {
+            { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+            { "n", "<c-k>",          actions.scroll_view(-0.25),             { desc = "Scroll the view up" } },
+            { "n", "<c-j>",          actions.scroll_view(0.25),              { desc = "Scroll the view down" } },
+            -- 使用 pcall 忽略 ]c / [c 到頭/到尾時的報錯
+            {
+              "n",
+              "]c",
+              function()
+                vim.cmd("wincmd l")
+                pcall(vim.cmd, "normal! ]c")
+                vim.cmd("wincmd h")
+              end,
+              { desc = "next diff" },
+            },
+            {
+              "n",
+              "[c",
+              function()
+                vim.cmd("wincmd l")
+                pcall(vim.cmd, "normal! [c")
+                vim.cmd("wincmd h")
+              end,
+              { desc = "previous diff" },
+            },
+          },
+        },
         hooks = {
           -- 在進入 diff 視窗時自動展開所有摺疊
-          diff_buf_read = function(bufnr)
-            vim.opt_local.foldlevel = 99
+          diff_buf_win_enter = function(bufnr, winid, ctx)
+            vim.opt_local.foldenable = false
           end,
         },
       })

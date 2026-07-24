43d9a751b40e2ac321da03dd640dd6f68a056fd0
Uris Cheng <cheng.uris@inventec.com>
2026-07-25 00:31:15 +0800

    fix: auto enable TS highlight

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,23 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
+    local ts = require("nvim-treesitter")
+    
+    -- 設定編譯器
     require("nvim-treesitter.install").compilers = { "zig", "clang", "gcc" }
+
+    -- 自動安裝所需的語法解析器
+    ts.install({
+      "c", "lua", "vim", "vimdoc", "query", "markdown", "gitcommit"
+    })
+
+    -- 當開啟檔案時啟用高亮
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
   end,
 }

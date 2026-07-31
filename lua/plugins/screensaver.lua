cea79fd1cfe6a5330e0630aba166d25b6bef5150
Uris Cheng <cheng.uris@inventec.com>
2026-07-31 17:08:57 +0800

    conf: config screensaver theme

--- lua/plugins/screensaver.lua
+++ lua/plugins/screensaver.lua
@@ -4,6 +4,21 @@ return {
   config = function()
     require("screensaver").setup({
       idle_ms = 180 * 1000, -- Idle time in milliseconds (5 minute)
+      animations = {
+          "matrix",
+          -- "rain",
+          -- "game_of_life",
+          -- "move_left",
+          -- "move_right",
+          "scramble",
+          -- "random_case",
+          -- "bounce",
+          "starfield",
+          -- "pipes",
+          -- "fire",
+          -- "snow",
+          -- "zoo",
+        },
     })
   end,
 }

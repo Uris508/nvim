f64def73693dc16d060fd6e5a7eb20dd8871313d
Uris Cheng <cheng.uris@inventec.com>
2026-09-03 11:36:55 +0800

    fix: codesnap.nvim breadcrumbs will not show base on cwd

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -4,10 +4,10 @@ return
    event = "VeryLazy",
   -- build = "make",
   keys = {
-    { "<leader>cp", "ggVG:<C-u>'<,'>CodeSnap<CR>", mode = "n", desc = "Copy code snapshot" },
-    { "<leader>cp", ":'<,'>CodeSnap<cr>", mode = "x", desc = "Copy code snapshot" },
-    { "<leader>cP", "ggVG:<C-u>'<,'>CodeSnapHighlight<cr>", mode = "n", desc = "Copy code snapshot Hightlight" },
-    { "<leader>cP", ":'<,'>CodeSnapHighlight<cr>", mode = "x", desc = "Copy code snapshot Hightlight" },
+    { "<leader>cp", "<cmd>set noshellslash<CR>ggVG:<C-u>'<,'>CodeSnap<CR>", mode = "n", desc = "Copy code snapshot" },
+    { "<leader>cp", "<cmd>set noshellslash<CR>:'<,'>CodeSnap<cr>", mode = "x", desc = "Copy code snapshot" },
+    { "<leader>cP", "<cmd>set noshellslash<CR>ggVG:<C-u>'<,'>CodeSnapHighlight<cr>", mode = "n", desc = "Copy code snapshot Hightlight" },
+    { "<leader>cP", "<cmd>set noshellslash<CR>:'<,'>CodeSnapHighlight<cr>", mode = "x", desc = "Copy code snapshot Hightlight" },
   },
   opts = {
     snapshot_config = {
@@ -23,8 +23,8 @@ return
           color = "#00000040",
         },
         margin = {
-          x = 0,
-          y = 0,
+          x = 20,
+          y = 20,
         },
       },
       code_config = {
